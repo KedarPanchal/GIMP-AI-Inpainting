43fdab840105a459e47e8ce5be7dfd01e1ae1a99
KedarPanchal <kedar.t.panchal@outlook.com>
2025-01-25 00:04:31 -0600

    Fix test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #! /bin/zsh
 
-cp "ai-integration.py" "Users/kpanchal/Library/Application Support/Gimp/3.0/plug-ins/ai-integration/ai-integration.py"
-chmod +x "Users/kpanchal/Library/Application Support/Gimp/3.0/plug-ins/ai-integration/ai-integration.py"
+cp "ai-integration.py" "/Users/kpanchal/Library/Application Support/Gimp/3.0/plug-ins/ai-integration/ai-integration.py"
+chmod +x "/Users/kpanchal/Library/Application Support/Gimp/3.0/plug-ins/ai-integration/ai-integration.py"
 /Applications/Gimp.app/Contents/MacOS/gimp --verbose
