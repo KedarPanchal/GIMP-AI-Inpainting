430685d810263aab8a9b646df9cb2520cc4e7d88
KedarPanchal <kedar.t.panchal@outlook.com>
2025-02-23 17:29:25 -0600

    Revert "Figured out the issue at least and commented where it is"
    This reverts commit d2acf8040be7d58541e2921009bbce84a752a375.

--- ai-integration.py
+++ ai-integration.py
@@ -19,7 +19,6 @@ from gi.repository import Gio
 
 
 class AiIntegration(Gimp.PlugIn):
-    # Plugin initialization
     def do_query_procedures(self):
         return ["ai-integration"]
     
@@ -36,9 +35,6 @@ class AiIntegration(Gimp.PlugIn):
         return procedure 
 
     def inpaint(self, image, mask, **args):
-        """
-        """
-
         pipeline = diffusers.AutoPipelineForInpainting.from_pretrained("diffusers/stable-diffusion-xl-1.0-inpainting-0.1", torch_dtype=torch.float16, variant="fp16", safety_checker=None)
         
         if torch.cuda.is_available():
@@ -158,10 +154,8 @@ class AiIntegration(Gimp.PlugIn):
                 Gimp.Image.undo_group_start(image)
 
                 layer = Gimp.Layer.new_from_visible(image, image, "mask")
-                # The error is here, it's inserting the layer above the activte one instead of the top
                 Gimp.Image.insert_layer(image, layer, None, 0)
                 drawable = image.get_layers()[0]
-                print(drawable)
                 Gimp.Drawable.edit_fill(drawable, Gimp.FillType.WHITE)
                 Gimp.Selection.invert(image)
                 Gimp.Drawable.edit_fill(drawable, Gimp.FillType.WHITE)
@@ -189,7 +183,6 @@ class AiIntegration(Gimp.PlugIn):
                 for layer in Gimp.Image.get_layers(image):
                     if layer not in invisibles:
                         Gimp.Item.set_visible(layer, True)
-
                 Gimp.Image.undo_group_end(image)
 
                 self.inpaint(
