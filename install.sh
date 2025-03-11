e0fcf6703bc215ed515503bc72b771364573a4d6
KedarPanchal <kedar.t.panchal@outlook.com>
2025-03-11 13:50:24 -0500

    Improve user friendliness for MacOS installs using the bash script

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Read and change directories to where GIMP's Python version is
-read -p "GIMP 3.0 Python Path: " gimp_path
-gimp_path=$(echo "$gimp_path" | xargs)
-python_path="${gimp_path}/python"
-cd "$gimp_path"
+# Note: Do NOT run as sudo (causes whoami to break)!
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cd "/Applications/GIMP.app/Contents/MacOS"
+    python_path="/Applications/GIMP.app/Contents/MacOS/python"
+else
+    # Read and change directories to where GIMP's Python version is
+    read -p "GIMP 3.0 Python Path: " gimp_path
+    gimp_path=$(echo "$gimp_path" | xargs)
+    python_path="${gimp_path}/python"
+    cd "$gimp_path"
+fi
 
 # Install pip on GIMP Python
 sudo curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -17,8 +23,13 @@ $(echo "sudo ${python_path} -m pip install -r requirements.txt")
 sudo rm -rf "requirements.txt"
 
 # Download plugin to GIMP
-read -p "Enter GIMP Plug-Ins folder: " plug_in_folder
-plug_in_folder=$(echo "$plug_in_folder" | xargs)
+if [[ $OSTYPE == "darwin"* ]]; then
+    me=$(whoami)
+    plug_in_folder="/Users/${me}/Library/Application Support/Gimp/3.0/plug-ins"
+else
+    read -p "Enter GIMP Plug-Ins folder: " plug_in_folder
+    plug_in_folder=$(echo "$plug_in_folder" | xargs)
+fi
 cd "$plug_in_folder"
 sudo curl -sSL https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/ai-integration.py -o ai-integration.py
 mkdir ai-integration
