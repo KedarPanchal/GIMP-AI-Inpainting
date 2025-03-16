b66ace11d65b12621ee3dec7d472f924beaa0b27
KedarPanchal <kedar.t.panchal@outlook.com>
2025-03-16 15:32:06 -0500

    Make powershell script require less user input

--- install.ps1
+++ install.ps1
@@ -1,17 +1,13 @@
-$gimpPath = Read-Host -Prompt "GIMP 3.0 Python Path"
-$gimpPath.Trim()
-Set-Location -Path $gimpPath
+Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\bin"
 
 curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-& $gimpPath\python.exe get-pip.py
+& "$(Get-Location)\pythonw.exe" get-pip.py
 Remove-Item get-pip.py
 
 curl -sSL https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/requirements.txt -o requirements.txt
-& $gimpPath\python.exe -m pip install -r requirements.txt
+& "$(Get-Location)\pythonw.exe" -m pip install -r requirements.txt
 Remove-Item requirements.txt
 
-$plugIns = Read-Host -Prompt "Enter GIMP Plug-Ins folder"
-$plugIns.Trim()
-Set-Location -Path $plugIns
+Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\lib\gimp\3.0\plug-ins"
 curl -sSL https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/ai-integration.py -o ai-integration.py
 New-Item -Path $pwd.Path -Name "ai-integration" -ItemType Directory
