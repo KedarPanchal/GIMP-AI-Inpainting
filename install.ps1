01e2cf1a835d4079849ebcad28741e2facf3c8d6
KedarPanchal <kedar.t.panchal@outlook.com>
2025-03-16 15:48:47 -0500

    Fix python used

--- install.ps1
+++ install.ps1
@@ -1,11 +1,11 @@
 Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\bin"
 
 Invoke-WebRequest -Uri https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
-& "$(Get-Location)\pythonw.exe" get-pip.py
+& "$(Get-Location)\python.exe" get-pip.py
 Remove-Item get-pip.py
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/requirements.txt -OutFile requirements.txt
-& "$(Get-Location)\pythonw.exe" -m pip install -r requirements.txt
+& "$(Get-Location)\python.exe" -m pip install -r requirements.txt
 Remove-Item requirements.txt
 
 Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\lib\gimp\3.0\plug-ins"
