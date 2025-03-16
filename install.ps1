e21ddb853faf4d849bd543e9b9c2e582c91067b9
KedarPanchal <kedar.t.panchal@outlook.com>
2025-03-16 15:51:24 -0500

    Fix global install on GIMP

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ Invoke-WebRequest -Uri https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
 Remove-Item get-pip.py
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/requirements.txt -OutFile requirements.txt
-& "$(Get-Location)\python.exe" -m pip install -r requirements.txt
+& "$(Get-Location)\python.exe" -m pip install --break-system-packages -r requirements.txt
 Remove-Item requirements.txt
 
 Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\lib\gimp\3.0\plug-ins"
