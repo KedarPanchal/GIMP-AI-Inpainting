9d7a6d12513e69d19377b921aed73d6c7169cd9e
KedarPanchal <kedar.t.panchal@outlook.com>
2025-03-16 15:40:06 -0500

    Change cURL to Invoke-WebRequest

--- install.ps1
+++ install.ps1
@@ -1,13 +1,13 @@
 Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\bin"
 
-curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+Invoke-WebRequest -Uri https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
 & "$(Get-Location)\pythonw.exe" get-pip.py
 Remove-Item get-pip.py
 
-curl -sSL https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/requirements.txt -o requirements.txt
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/requirements.txt -OutFilerequirements.txt
 & "$(Get-Location)\pythonw.exe" -m pip install -r requirements.txt
 Remove-Item requirements.txt
 
 Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\lib\gimp\3.0\plug-ins"
-curl -sSL https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/ai-integration.py -o ai-integration.py
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/ai-integration.py -OutFileai-integration.py
 New-Item -Path $pwd.Path -Name "ai-integration" -ItemType Directory
