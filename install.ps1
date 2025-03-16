28696000421de97f125987409c495ff7b9c52e35
KedarPanchal <kedar.t.panchal@outlook.com>
2025-03-16 15:41:24 -0500

    Fix spacing errors

--- install.ps1
+++ install.ps1
@@ -4,10 +4,10 @@ Invoke-WebRequest -Uri https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py
 & "$(Get-Location)\pythonw.exe" get-pip.py
 Remove-Item get-pip.py
 
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/requirements.txt -OutFilerequirements.txt
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/requirements.txt -OutFile requirements.txt
 & "$(Get-Location)\pythonw.exe" -m pip install -r requirements.txt
 Remove-Item requirements.txt
 
 Set-Location -Path "C:\Users\$([Environment]::UserName)\AppData\Local\Programs\GIMP 3\lib\gimp\3.0\plug-ins"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/ai-integration.py -OutFileai-integration.py
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/KedarPanchal/GIMP-AI-Inpainting/refs/heads/main/ai-integration.py -OutFile ai-integration.py
 New-Item -Path $pwd.Path -Name "ai-integration" -ItemType Directory
